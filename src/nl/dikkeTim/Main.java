a08131f0b6998a2371bd8dccb5a1480b03759c5c
Steve Galili <steve@meetfeli.com>
2018-10-03 18:44:14 +0200

    copying files

--- src/nl/dikkeTim/Main.java
+++ src/nl/dikkeTim/Main.java
@@ -19,15 +19,22 @@ public class Main {
 
             ByteBuffer buffer = ByteBuffer.allocate(100);
             byte[] outputBytes = "Hello World!".getBytes();
+            buffer.put(outputBytes);
+            buffer.putInt(112);
+            buffer.putInt(-1212);
             byte[] outputBytes2 = "Hello Again".getBytes();
-            buffer.put(outputBytes).putInt(112).putInt(-1212).put(outputBytes2).putInt(119);
+            buffer.put(outputBytes2);
+            buffer.putInt(119);
             buffer.flip();
             binChannel.write(buffer);
+
+//            buffer.put(outputBytes).putInt(112).putInt(-1212).put(outputBytes2).putInt(119); <- dit ook mogelijk 'chainen'
 
             RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
             FileChannel channel = ra.getChannel();
 
             ByteBuffer readBuffer = ByteBuffer.allocate(100);
+
             channel.read(readBuffer);
             readBuffer.flip();
             byte[] inputString = new byte[outputBytes.length];
@@ -40,6 +47,15 @@ public class Main {
             System.out.println("inputString2 is: " + new String(inputString2));
             System.out.println("int3 is: " + readBuffer.getInt());
 
+            RandomAccessFile copyFile = new RandomAccessFile("dataCopy.dat", "rwd");
+            FileChannel copyChannel = copyFile.getChannel();
+            channel.position(0);
+            long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+            System.out.println("num transferred: " + numTransferred);
+
+            channel.close();
+            ra.close();
+            copyChannel.close();
 
         } catch(IOException e) {
             e.printStackTrace();
