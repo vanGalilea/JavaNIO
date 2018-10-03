28b9c0fcb25b9c495e74d4ef6e4ff16fa061ec06
Steve Galili <steve@meetfeli.com>
2018-10-03 17:45:08 +0200

    writing binary files

--- src/nl/dikkeTim/Main.java
+++ src/nl/dikkeTim/Main.java
@@ -1,6 +1,9 @@
 package nl.dikkeTim;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,15 +13,34 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        try (FileOutputStream binFile = new FileOutputStream("data.txt");
+             FileChannel binChannel = binFile.getChannel()) {
+
+            byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(315);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-315);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            Files.write(dataPath, "\nLine 4".getBytes("UTF-8"), StandardOpenOption.APPEND);
-            List<String> lines = Files.readAllLines(dataPath);
-            for (String line : lines) {
-                System.out.println(line);
-            }
+//            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+//            Files.write(dataPath, "\nLine 4".getBytes("UTF-8"), StandardOpenOption.APPEND);
+//            List<String> lines = Files.readAllLines(dataPath);
+//            for (String line : lines) {
+//                System.out.println(line);
+//            }
         } catch(IOException e) {
             e.printStackTrace();
         }
