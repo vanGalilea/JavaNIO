eea2ffa46af3e31d987ef046da74993c48772f2a
Steve Galili <steve@meetfeli.com>
2018-10-03 18:05:43 +0200

    reading binary files continued

--- src/nl/dikkeTim/Main.java
+++ src/nl/dikkeTim/Main.java
@@ -2,6 +2,7 @@ package nl.dikkeTim;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
@@ -13,7 +14,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        try (FileOutputStream binFile = new FileOutputStream("data.txt");
+        try (FileOutputStream binFile = new FileOutputStream("data.dat");
              FileChannel binChannel = binFile.getChannel()) {
 
             byte[] outputBytes = "Hello World!".getBytes();
@@ -33,14 +34,31 @@ public class Main {
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
 
-//            FileInputStream file = new FileInputStream("data.txt");
-//            FileChannel channel = file.getChannel();
-//            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-//            Files.write(dataPath, "\nLine 4".getBytes("UTF-8"), StandardOpenOption.APPEND);
-//            List<String> lines = Files.readAllLines(dataPath);
-//            for (String line : lines) {
-//                System.out.println(line);
-//            }
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+            buffer.flip();
+            System.out.println("outputBytes: " +  new String(outputBytes));
+            long numBytedRead = channel.read(buffer);
+            if(buffer.hasArray()) {
+                System.out.println("byte buffer is: " + new String(buffer.array()));
+            }
+            numBytedRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+            intBuffer.flip();
+            numBytedRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+
+            channel.close();
+            ra.close();
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            byte[] b = new byte[outputBytes.length];
+//            ra.read(b);
+//            System.out.println(new String(b));
+//
+//            long int1 = ra.readInt();
+//            long int2 = ra.readInt();
+//            System.out.println(int1 + " " + int2);
+
         } catch(IOException e) {
             e.printStackTrace();
         }
